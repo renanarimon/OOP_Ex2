ebe44834aa10a000b2b49481977c2349ab009256
renanarimon <77155986+renanarimon@users.noreply.github.com>
2021-12-05 00:13:45 +0200

    save

---
+++ src/api/Node.java
@@ -0,0 +1,70 @@
+package api;
+
+
+
+public class Node implements NodeData {
+    private GeoLocation location;
+    private int key;
+    private int tag;
+    private double weight;
+    private String info;
+
+
+//    enum color{
+//        WHITE,
+//        GRAY,
+//        BLACK
+//    }
+
+    public Node(GeoLocation location, int key) {
+        this.location = location;
+        this.key = key;
+        this.tag = 0;
+        this.info = "";
+    }
+
+    @Override
+    public int getKey() {
+        return this.key;
+    }
+
+    @Override
+    public GeoLocation getLocation() {
+        return this.location;
+    }
+
+    @Override
+    public void setLocation(GeoLocation p) {
+        this.location=p;
+    }
+
+    @Override
+    public double getWeight() {
+        return this.weight;
+    }
+
+    @Override
+    public void setWeight(double w) {
+        this.weight = w;
+    }
+
+    @Override
+    public String getInfo() {
+        return this.info;
+    }
+
+    @Override
+    public void setInfo(String s) {
+        this.info = s;
+    }
+
+    @Override
+    public int getTag() {
+        return this.tag;
+    }
+
+    @Override
+    public void setTag(int t) {
+        this.tag = t;
+    }
+}
